15810e69f62b1753bf8747508c4e8789100b6a80
M. Chae <cmirs56@gmail.com>
2020-04-04 19:42:47 +0900

    code_survey result

---
+++ survey_result.R
@@ -0,0 +1,122 @@
+
+#### loading the library and data 
+library(tidyverse)
+library(readr)
+library(car)
+read.csv.any <- function(text, sep = "", ...) {
+  encoding <- as.character(guess_encoding(text)[1,1])
+  setting <- as.character(tools::file_ext(text))
+  if(sep != "" | !(setting  %in% c("csv", "txt")) ) setting <- "custom"
+  separate <- list(csv = ",", txt = "\n", custom = sep)
+  result <- read.table(text, sep = separate[[setting]], fileEncoding = encoding, ...)
+  return(result)
+}
+df <- read.csv.any("c:/Users/MC/R/survey_telework/survey_num.csv", header = TRUE, na.strings = c("NA", ""))
+df$efficiency <- recode(df$efficiency, "'100' = 3 ; '100~80' =  2 ; '150~100' = 4")
+df$efficiency <- as.numeric(df$efficiency)
+
+#### outline of the dataframe  
+str(df)
+head(df)
+summary(df)
+
+#### mean subset
+df_stfc <- df %>% 
+  group_by(position, responsibility) %>% 
+  summarise(mean_stfc = mean(satisfaction, na.rm = TRUE))
+df_stfc
+
+df_prvc <- df %>% 
+  group_by(position, responsibility) %>% 
+  summarise(mean_prvc = mean(privacy, na.rm = TRUE))
+df_prvc
+
+df_effi <- df %>% 
+  group_by(position, responsibility) %>% 
+  summarise(mean_effi = mean(efficiency, na.rm = TRUE))
+df_effi
+
+df_effi_days <- df %>% 
+  group_by(days, position, responsibility) %>% 
+  summarise(mean_effi = mean(efficiency, na.rm = TRUE))
+df_effi_days
+
+df_coop <- df %>% 
+  group_by(position, responsibility) %>% 
+  summarise(mean_coop = mean(cooperation))
+df_coop
+
+df_coop_days <- df %>% 
+  group_by(days, position, responsibility) %>% 
+  summarise(mean_coop = mean(cooperation))
+df_coop_days
+
+#### overall 
+df_all <- tibble(
+  `satisfaction` = mean(df_stfc$mean_stfc), 
+  `efficiency` = mean(df_effi$mean_effi), 
+  `cooperation` = mean(df_coop$mean_coop),
+  `privacy` = mean(df_prvc$mean_prvc) 
+)
+df_all
+df_all_t <- t(df_all) #switching_row & column 
+df_all_t
+str(df_all_t)
+df_all_df <- as.data.frame(df_all_t)
+
+ggplot(df_all_df, aes([1,1],V1))+
+  geom_col(position = "dodge")     #unsolved
+
+#### mean_coop/effi & position & responsibility 
+ggplot(df_coop, aes(position, mean_coop, fill = responsibility))+
+  geom_col(position = "dodge")
+
+ggplot(df_coop_days, aes(days, mean_coop, fill = position))+
+  geom_col(position = "dodge")
+
+ggplot(df_effi, aes(position, mean_effi, fill = responsibility))+
+  geom_col(position = "dodge")
+
+ggplot(df_effi_days, aes(days, mean_effi, fill = position))+
+  geom_col(position = "dodge")
+
+#### the relation between efficiency and cooperation 
+ggplot(df, aes(efficiency, cooperation))+
+  geom_point(mapping = aes(color = days, shpae = days))+
+  geom_smooth(color = 'black', fill = 'grey')+
+  facet_wrap(~position)
+
+#### days & position & respinsibility & efficiency/cooperation
+ggplot(df_coop_days, aes(days, mean_coop))+
+  geom_point(mapping = aes(color = position, shape = responsibility))+
+  geom_boxplot()+
+  theme_bw()
+
+ggplot(df_effi_days, aes(days, mean_effi))+
+  geom_point(mapping = aes(color = position, shape = responsibility))+
+  geom_boxplot()+
+  theme_bw()
+
+
+
+
+#### improvement 
+df_imprv <- df %>% 
+  select(improvement, responsibility) 
+df_imprv  
+
+ggplot(df_imprv, aes(improvement, fill = responsibility))+
+  geom_bar(position = "dodge")+
+  coord_flip()
+
+# 상관분석
+df_narm <- na.omit(df)
+cor(df[,c(3:6)],method=c("pearson"))
+
+library(PerformanceAnalytics)
+chart.Correlation(df_narm[,c(3:6)],pch=19)
+
+df_0days <- df %>% 
+filter(df$days == "0일")
+df_0days
+nrow(df_0days)           
---
+++ survey_wordcloud.R
@@ -0,0 +1,66 @@
+
+library(KoNLP) #loading the required libraries for wordcloud 
+library(wordcloud)
+library(stringr)
+library(XML)
+library(rvest)
+library(tm)
+library(igraph)
+library(wordcloud2)
+library(arules)
+library(combinat)
+library(multilinguer)
+
+.libPaths()
+useSejongDic()
+useNIADic() 
+
+comments <- readLines("c:/users/MC/R/survey_telework/comments.txt", encoding = "UTF-8")
+head(comments, 10)
+
+comments <- paste0(comments, collapse = "")
+
+nouns <- extractNoun(comments)
+nouns1 <- unlist(nouns)
+nouns2 <- Filter(function(x) {nchar(x) <= 20 & nchar(x) > 1}, nouns1)
+
+nouns2 <- gsub(" ","", nouns2)
+nouns2 <- gsub("<.+?>|\t", "", nouns2)
+nouns2 <- gsub("\\d+","" ,nouns2)	
+nouns2 <- gsub("\\.","" ,nouns2) 
+nouns2 <- gsub("재택근무","" ,nouns2)
+nouns2 <- gsub("생각","" ,nouns2)
+nouns2 <- gsub("업무","" ,nouns2)
+nouns2 <- gsub("근무","" ,nouns2)
+nouns2 <- gsub("재택","" ,nouns2)
+nouns2 <- gsub("하기","" ,nouns2)
+nouns2 <- gsub("관련","" ,nouns2)
+nouns2 <- gsub("들이","" ,nouns2)
+str(nouns2)
+
+nouns3 <- Filter(function(x) {nchar(x) <= 20 & nchar(x) > 1}, nouns2)
+
+wordcount <- table(nouns3)
+head(sort(wordcount, decreasing = TRUE), 50)
+
+windowsFonts(malgun=windowsFont("맑은고딕"))
+wordcloud2(data=wordcount, minSize = 5, 
+           minRotation = 0, maxRotation = 0, rotateRatio = 1, 
+           fontFamily='맑은고딕',size=0.8)
+
+
+
+#### not so beautiful 
+palete <- brewer.pal(9, "Set3")
+wordcloud2(names(wordcount), freq = wordcount, scale = c(5,1),
+          rot.per = 0.25, min.freq = 1, random.order = F, 
+          random.color = T, colors = palete)
+
+#### don't understand
+txt <- readLines("c:/users/R/survey/del_gsub.txt", encoding = "UTF-8")
+cnt_txt <- length(txt)
+for (i in 1:cnt_txt){
+  nouns2 <- gsub((txt[i]), "", nouns2)
+}
+
+
